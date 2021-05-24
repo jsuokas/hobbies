adf62e61975bc5e5b5f793f3d68bf8c37df2ab34
Jukka Suokas <jukka.suokas@reaktor.com>
2021-05-24 21:40:39 +0300

    Add pure css and feather icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import gear from './gear.json'
 import './App.css';
+import FeatherIcon from 'feather-icons-react';
 
 const consumablesWeight = {
+  water: 1,
   gasBottleSmall: 0.1,
   gasBottleMedium: 0.230,
   toiletPaper: 0.1,
@@ -35,7 +37,7 @@ function App() {
   const [consumablesGasMedium, setConsumablesGasMedium] = useState(0)
   const [consumablesTurmatMeals, setConsumablesTurmatMeals] = useState(0)
   const gearItems = gear.filter(it => gearTypeFilters.includes(it.type))
-  const totalConsumablesWeight = consumablesWater + (consumablesGasSmall * consumablesWeight.gasBottleSmall) + (consumablesGasMedium * consumablesWeight.gasBottleMedium) + (consumablesToiletPaper * consumablesWeight.toiletPaper) + (consumablesToiletWetWipes * consumablesWeight.wetWipes) + (consumablesTurmatMeals * consumablesWeight.turmatMeal)
+  const totalConsumablesWeight = (consumablesWater * consumablesWeight.water) + (consumablesGasSmall * consumablesWeight.gasBottleSmall) + (consumablesGasMedium * consumablesWeight.gasBottleMedium) + (consumablesToiletPaper * consumablesWeight.toiletPaper) + (consumablesToiletWetWipes * consumablesWeight.wetWipes) + (consumablesTurmatMeals * consumablesWeight.turmatMeal)
   const totalWeight = gearItems
     .filter(it => gearTypeFilters.includes(it.type))
     .filter(it => gearItemFilters.filter(item => item.name === it.name).length !== 0)
@@ -45,85 +47,131 @@ function App() {
     <div className="App">
       <div className="title"><h4>What kind of trip?</h4></div>
       <div className="content">
+      <table class="pure-table">
+          <tbody>
       {gear
         .map(it => it.type)
         .reduce((previous, current) => previous.includes(current) ? previous : [...previous, current], [])
-        .map(gearType => {
+        .map((gearType, index) => {
           const isSelected = gearTypeFilters.includes(gearType)
           const id = `gear-filter-stage-1-${gearType}`
           const onChange = () => isSelected ? setGearTypeFilters(gearTypeFilters.filter(it => it !== gearType)) : setGearTypeFilters([...gearTypeFilters, gearType])
+          const odd = index % 2 !== 0
 
           return (
-            <div key={`key-${id}`} className="gear-item">
-              <input
-              id={id}
-              type="checkbox"
-              value={gearType}
-              name={gearType}
-              checked={isSelected}
-              onChange={onChange} />
-              <label htmlFor={id}>{typeToDescription[gearType] ? typeToDescription[gearType] : gearType}</label>
-            </div>
+            <tr className={odd ?  'pure-table-odd' : undefined}  key={`key-${id}`}>
+              <td>
+                <input
+                  id={id}
+                  type="checkbox"
+                  value={gearType}
+                  name={gearType}
+                  checked={isSelected}
+                  onChange={onChange} />
+              </td>
+              <td>
+                <label htmlFor={id}>{typeToDescription[gearType] ? typeToDescription[gearType] : gearType}</label>
+              </td>
+            </tr>
           )
         })}
+          </tbody>
+        </table>
       </div>
 
-      <div className="title"><h4>Base weight</h4><input className="select-all-button" type="button" value="Select all" onClick={() => setGearItemFilters(gearItems)} /></div>
-      <div className="content">
-      
-      {gearItems.map(item => {
-        const id = `gear-filter-stage-2-${item.name}`
-        const isSelected = gearItemFilters.filter(it => it.name === item.name).length !== 0 ? true : false 
-        const onChange = () => isSelected ? setGearItemFilters(gearItemFilters.filter(it => it.name !== item.name)) : setGearItemFilters([...gearItemFilters, item])
+      <div className="title"><h4>Base weight</h4><button className="button-secondary pure-button" onClick={() => setGearItemFilters(gearItems)}>Select all</button></div>
+      <table class="pure-table">
+          <tbody>
+            {gearItems.map((item, index) => {
+              const id = `gear-filter-stage-2-${item.name}`
+              const isSelected = gearItemFilters.filter(it => it.name === item.name).length !== 0 ? true : false 
+              const onChange = () => isSelected ? setGearItemFilters(gearItemFilters.filter(it => it.name !== item.name)) : setGearItemFilters([...gearItemFilters, item])
+              const odd = index % 2 !== 0
 
-        return (
-          <div key={`key-${id}`} className="gear-item">
-            <input
-                id={id}
-                type="checkbox"
-                value={item.name}
-                name={item.name}
-                checked={isSelected}
-                onChange={onChange} />
-                <label htmlFor={id}>{item.name} ({item.weight})</label>
-          </div>
-        )
-      })}
-      </div>
+              return (
+                <tr className={odd ?  'pure-table-odd' : undefined} key={`key-${id}`}>
+                  <td>
+                  <input
+                    id={id}
+                    type="checkbox"
+                    value={item.name}
+                    name={item.name}
+                    checked={isSelected}
+                    onChange={onChange} />
+                  </td>
+                  <td><label htmlFor={id}>{item.name}</label></td>
+                  <td>{item.weight}</td>
+              </tr>
+              )
+            })}
+          </tbody>
+      </table>
 
       <div className="title"><h4>Consumables</h4></div>
-      <div className="content">
-        <div>
-          <input type="button" value="+" onClick={() => setConsumablesWater(consumablesWater + 1)} />
-          <input type="button" value="-" onClick={() => setConsumablesWater(consumablesWater > 0 ? consumablesWater - 1 : 0)} />
-          <span className="consumables-label">{consumablesWater} liters of water</span>
-        </div>
-        <div>
-          <input type="button" value="+" onClick={() => setConsumablesGasSmall(consumablesGasSmall + 1)} />
-          <input type="button" value="-" onClick={() => setConsumablesGasSmall(consumablesGasSmall > 0 ? consumablesGasSmall - 1 : 0)} />
-          <span className="consumables-label">{consumablesGasSmall} small gas bottles (100g)</span>
-        </div>
-        <div>
-          <input type="button" value="+" onClick={() => setConsumablesGasMedium(consumablesGasMedium + 1)} />
-          <input type="button" value="-" onClick={() => setConsumablesGasMedium(consumablesGasMedium > 0 ? consumablesGasMedium - 1 : 0)} />
-          <span className="consumables-label">{consumablesGasMedium} medium gas bottles (256g)</span>
-        </div>
-        <div>
-          <input type="button" value="+" onClick={() => setConsumablesTurmatMeals(consumablesTurmatMeals + 1)} />
-          <input type="button" value="-" onClick={() => setConsumablesTurmatMeals(consumablesTurmatMeals> 0 ? consumablesTurmatMeals - 1 : 0)} />
-          <span className="consumables-label">{consumablesTurmatMeals} packs of Turmat meals</span>
-        </div>
-        <div>
-          <input type="button" value="+" onClick={() => setConsumablesToiletPaper(consumablesToiletPaper + 1)} />
-          <input type="button" value="-" onClick={() => setConsumablesToiletPaper(consumablesToiletPaper > 0 ? consumablesToiletPaper - 1 : 0)} />
-          <span className="consumables-label">{consumablesToiletPaper} rolls of toilet paper</span>
-        </div>
-        <div>
-          <input type="button" value="+" onClick={() => setConsumablesToiletWetWipes(consumablesToiletWetWipes + 1)} />
-          <input type="button" value="-" onClick={() => setConsumablesToiletWetWipes(consumablesToiletWetWipes > 0 ? consumablesToiletWetWipes - 1 : 0)} />
-          <span className="consumables-label">{consumablesToiletWetWipes} packs of wet wipes (50pc)</span>
-        </div>
-      </div>
+      <table class="pure-table">
+          <tbody>
+            <tr>
+              <td>
+                <button onClick={() => setConsumablesWater(consumablesWater + 1)}><FeatherIcon icon="plus-circle" /></button>
+                <button onClick={() => setConsumablesWater(consumablesWater > 0 ? consumablesWater - 1 : 0)}><FeatherIcon icon="minus-circle" /></button>
+              </td>
+              <td>
+                <span className="consumables-label">{consumablesWater} liter of water</span>
+              </td>
+              <td>{consumablesWeight.water}</td>
+            </tr>
+            <tr className="pure-table-odd">
+              <td>
+                <button  onClick={() => setConsumablesGasSmall(consumablesGasSmall + 1)}><FeatherIcon icon="plus-circle" /></button>
+                <button onClick={() => setConsumablesGasSmall(consumablesGasSmall > 0 ? consumablesGasSmall - 1 : 0)}><FeatherIcon icon="minus-circle" /></button>
+              </td>
+              <td>
+                <span className="consumables-label">{consumablesGasSmall} small gas bottles)</span>
+              </td>
+              <td>{consumablesWeight.gasBottleSmall}</td>
+            </tr>
+            <tr>
+              <td>
+                <button  onClick={() => setConsumablesGasMedium(consumablesGasMedium + 1)}><FeatherIcon icon="plus-circle" /></button>
+                <button  onClick={() => setConsumablesGasMedium(consumablesGasMedium > 0 ? consumablesGasMedium - 1 : 0)}><FeatherIcon icon="minus-circle" /></button>
+              </td>
+              <td>
+                <span className="consumables-label">{consumablesGasMedium} medium gas bottles (256g)</span>
+              </td>
+              <td>{consumablesWeight.gasBottleMedium}</td>
+            </tr>
+            <tr className="pure-table-odd">
+              <td>
+                <button  onClick={() => setConsumablesTurmatMeals(consumablesTurmatMeals + 1)}><FeatherIcon icon="plus-circle" /></button>
+                <button  onClick={() => setConsumablesTurmatMeals(consumablesTurmatMeals> 0 ? consumablesTurmatMeals - 1 : 0)}><FeatherIcon icon="minus-circle" /></button>
+              </td>
+              <td>
+                <span className="consumables-label">{consumablesTurmatMeals} packs of Turmat meals</span>
+              </td>
+              <td>{consumablesWeight.turmatMeal}</td>
+            </tr>
+            <tr>
+              <td>
+                <button  onClick={() => setConsumablesToiletPaper(consumablesToiletPaper + 1)}><FeatherIcon icon="plus-circle" /></button>
+                <button  onClick={() => setConsumablesToiletPaper(consumablesToiletPaper > 0 ? consumablesToiletPaper - 1 : 0)}><FeatherIcon icon="minus-circle" /></button>
+              </td>
+              <td>
+                <span className="consumables-label">{consumablesToiletPaper} rolls of toilet paper</span>
+              </td>
+              <td>{consumablesWeight.toiletPaper}</td>
+            </tr>
+            <tr className="pure-table-odd">
+              <td>
+                <button  onClick={() => setConsumablesToiletWetWipes(consumablesToiletWetWipes + 1)}><FeatherIcon icon="plus-circle" /></button>
+                <button onClick={() => setConsumablesToiletWetWipes(consumablesToiletWetWipes > 0 ? consumablesToiletWetWipes - 1 : 0)}><FeatherIcon icon="minus-circle" /></button>
+              </td>
+              <td>
+                <span className="consumables-label">{consumablesToiletWetWipes} packs of wet wipes (50pc)</span>
+              </td>
+              <td>{consumablesWeight.wetWipes}</td>
+            </tr>
+          </tbody>
+      </table>
 
       <div className="title"><strong>Total</strong> {totalWeight.toFixed(2)}kg</div>
     </div>
